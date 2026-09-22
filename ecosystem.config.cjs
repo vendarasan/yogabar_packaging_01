bea4df0e6510bed786386c00ded50748c50b4bc0
unknown <vendarasan25@gmail.com>
2026-09-23 02:10:13 +0530

    fix(pm2): simplify ecosystem config to use default pm2 log paths

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,17 +6,16 @@ module.exports = {
       script: 'index.js',
       instances: 1,
       exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5001
+      },
       env_production: {
         NODE_ENV: 'production',
         PORT: 5001
       },
       watch: false,
-      max_memory_restart: '1G',
-      exp_backoff_restart_delay: 100,
-      error_file: './logs/pm2-error.log',
-      out_file: './logs/pm2-out.log',
-      merge_logs: true,
-      time: true
+      max_memory_restart: '1G'
     }
   ]
 };
